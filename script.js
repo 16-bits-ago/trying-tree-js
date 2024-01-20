5718b28cdf0c2eae0067925e4b2b4e065928ab61
Vlad Kapustsin <kapustsin.u@titan-t.com>
2024-01-20 18:36:11 +0200

    move and scale objects

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ import './style.css';
 
 const scene = new THREE.Scene(); // СЦЕНА
 
+/* const axesHelper = new THREE.AxesHelper( 3 ); // Отображение осей x, y, z идущих из центра сцены
+scene.add( axesHelper ); */
+
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
@@ -20,20 +23,35 @@ document.body.appendChild( renderer.domElement );
 
 // Геометрия + материал дают нам сетку
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 'purple' } );
+const material = new THREE.MeshBasicMaterial( { color: 'purple', wireframe: true } ); // wireframe: true - делает объект прозрачным
 const cube = new THREE.Mesh( geometry, material );
 
 // Добавление сетки на сцену
 scene.add( cube );
 
 // Отодвигаем камеру от объекта, так как по дефолту камера и сам объект создаются в позиции 0
-camera.position.z = 2;
+camera.position.z = 5;
 
 function animate() {
 	requestAnimationFrame( animate );
 
+    // Виды преобразования обхектов: position, rotation, scale, quaternion
 	cube.rotation.x += 0.01;
 	cube.rotation.y += 0.01;
+
+    cube.position.x = -1;
+    cube.position.y = -0.5;
+    cube.position.z = -2;
+
+    cube.scale.x = 3;
+    cube.scale.y = 5;
+    cube.scale.z = 1;
+
+    /* Полезные методы для позиции:
+        cube.position.length() - расстояние между объектами
+        cube.position.distanceTo(camera.position) - расстояние между объектами в камере
+        cube.position.normalize() - уменьшает длину вектора до 1, но сохраняет его направление
+        cube.position.set(-1, -0.8, 0.5) изменение x, y, z*/
 
 	renderer.render( scene, camera );
 }
